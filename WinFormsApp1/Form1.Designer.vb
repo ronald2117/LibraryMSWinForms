a2e8a4cf541ca72f376151077aec143e4e136f89
ronald2117 <abel.ronald2117@gmail.com>
2024-11-15 12:17:19 +0800

    version 2

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -23,28 +23,85 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         btnTest = New Button()
+        LoginBtn = New Button()
+        Password = New TextBox()
+        Label2 = New Label()
+        Username = New TextBox()
+        Label1 = New Label()
         SuspendLayout()
         ' 
         ' btnTest
         ' 
-        btnTest.Location = New Point(310, 179)
+        btnTest.Location = New Point(111, 71)
         btnTest.Name = "btnTest"
-        btnTest.Size = New Size(134, 56)
+        btnTest.Size = New Size(125, 38)
         btnTest.TabIndex = 0
-        btnTest.Text = "MYSQL TEST"
+        btnTest.Text = "Connection Test"
         btnTest.UseVisualStyleBackColor = True
+        ' 
+        ' LoginBtn
+        ' 
+        LoginBtn.Location = New Point(315, 237)
+        LoginBtn.Name = "LoginBtn"
+        LoginBtn.Size = New Size(75, 23)
+        LoginBtn.TabIndex = 10
+        LoginBtn.Text = "Login"
+        LoginBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Password
+        ' 
+        Password.Location = New Point(302, 176)
+        Password.Name = "Password"
+        Password.Size = New Size(100, 23)
+        Password.TabIndex = 9
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(236, 184)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(57, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "Password"
+        ' 
+        ' Username
+        ' 
+        Username.Location = New Point(302, 138)
+        Username.Name = "Username"
+        Username.Size = New Size(100, 23)
+        Username.TabIndex = 7
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(236, 141)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(60, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Username"
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(LoginBtn)
+        Controls.Add(Password)
+        Controls.Add(Label2)
+        Controls.Add(Username)
+        Controls.Add(Label1)
         Controls.Add(btnTest)
         Name = "Form1"
         Text = "MYSQL DATABASE CONNECTION"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents btnTest As Button
+    Friend WithEvents LoginBtn As Button
+    Friend WithEvents Password As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Username As TextBox
+    Friend WithEvents Label1 As Label
 
 End Class
--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,4 +1,5 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.Diagnostics.Eventing
+Imports MySql.Data.MySqlClient
 Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
@@ -9,6 +10,36 @@ Public Class Form1
         Try
             conn.Open()
             MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=test"
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "SELECT * FROM Accounts WHERE username = '" & Username.Text & "' AND password = '" & Password.Text & "'"
+            COMMAND = New MySqlCommand(Query, conn)
+            Dim count As Integer
+            READER = COMMAND.ExecuteReader
+            Count = 0
+            While READER.Read
+                count = count + 1
+            End While
+            If count = 1 Then
+                MessageBox.Show("Login successful!")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and password are duplicate!")
+            Else
+                MessageBox.Show("Username and password are incorrect!")
+            End If
+
             conn.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
